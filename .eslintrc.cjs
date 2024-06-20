1de319c156c6310f200df6ce427330ffa7753cf2
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2024-06-20 17:58:06 -0600

    spin up base functionality

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,57 +2,57 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
-    // "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    // "plugin:json/recommended",
-    "plugin:react/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     // 0: no warning
     // 1: warning, fix optional
     // 2: error, must fix
-    "linebreak-style": "off",
-    "consistent-return": 0,
-    "no-debugger": 1,
-    "no-console": 1,
-    "padding-line-between-statements": [
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': 'off',
+    'consistent-return': 0,
+    'no-debugger': 1,
+    'no-console': 1,
+    'padding-line-between-statements': [
       2,
       // Always require blank lines after import, except between imports
-      { blankLine: "always", prev: "import", next: "*" },
-      { blankLine: "any", prev: "import", next: "import" },
+      { blankLine: 'always', prev: 'import', next: '*' },
+      { blankLine: 'any', prev: 'import', next: 'import' },
       // Always require blank lines before and after every sequence of variable declarations and export
       {
-        blankLine: "always",
-        prev: "*",
-        next: ["const", "let", "var", "export"],
+        blankLine: 'always',
+        prev: '*',
+        next: ['const', 'let', 'var', 'export'],
       },
       {
-        blankLine: "always",
-        prev: ["const", "let", "var", "export"],
-        next: "*",
+        blankLine: 'always',
+        prev: ['const', 'let', 'var', 'export'],
+        next: '*',
       },
       {
-        blankLine: "any",
-        prev: ["const", "let", "var", "export"],
-        next: ["const", "let", "var", "export"],
+        blankLine: 'any',
+        prev: ['const', 'let', 'var', 'export'],
+        next: ['const', 'let', 'var', 'export'],
       },
       // Always require blank lines before return statements
-      { blankLine: "always", prev: "*", next: "return" },
+      { blankLine: 'always', prev: '*', next: 'return' },
     ],
-    quotes: [2, "single", { allowTemplateLiterals: true }],
-    "jsx-quotes": [2, "prefer-double"],
-    "prefer-destructuring": [
-      "error",
+    quotes: [2, 'single', { allowTemplateLiterals: true }],
+    'jsx-quotes': [2, 'prefer-double'],
+    'prefer-destructuring': [
+      'error',
       {
         array: false,
         object: true,
@@ -61,39 +61,39 @@ module.exports = {
         enforceForRenamedProperties: false,
       },
     ],
-    "no-multiple-empty-lines": [2, { max: 1 }],
+    'no-multiple-empty-lines': [2, { max: 1 }],
 
     //// '@typescript-eslint' plugin
-    "@typescript-eslint/ban-ts-comment": 0,
-    "@typescript-eslint/default-param-last": 0,
-    "@typescript-eslint/explicit-module-boundary-types": 0,
-    "@typescript-eslint/no-unused-vars": [1, { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/naming-convention": [
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/default-param-last': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/naming-convention': [
       // these are taken straight from the airbnb-eslint-typescript config
-      "error",
+      'error',
       {
-        selector: "variable",
-        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
       },
       {
         // except for this one
-        selector: "variable",
-        modifiers: ["destructured"],
+        selector: 'variable',
+        modifiers: ['destructured'],
         format: null,
       },
       {
-        selector: "typeLike",
-        format: ["PascalCase"],
+        selector: 'typeLike',
+        format: ['PascalCase'],
       },
       {
-        selector: "function",
-        format: ["camelCase", "PascalCase"],
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
       },
     ],
-    "@typescript-eslint/consistent-type-imports": 2,
-    "@typescript-eslint/sort-type-constituents": 2,
+    '@typescript-eslint/consistent-type-imports': 2,
+    '@typescript-eslint/sort-type-constituents': 2,
 
     // prettier extension
-    "prettier/prettier": [2, { singleQuote: true }],
+    'prettier/prettier': [2, { singleQuote: true }],
   },
 };
--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,6 +1,21 @@
-const TextEditor = () =>  {
-  return <textarea
-  rows={80} cols={100}/>
-}
+import { useState } from 'react';
 
-export  default TextEditor
+const TextEditor = () => {
+  const [input, setInput] = useState('');
+
+  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <div>
+      <textarea rows={30} cols={70} value={input} onChange={handleInput} />
+      <div>
+        <button onClick={() => setInput('')}>Clear All</button>
+        <button>Save</button>
+      </div>
+    </div>
+  );
+};
+
+export default TextEditor;
