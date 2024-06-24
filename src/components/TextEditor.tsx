560d50a3b8b3cdb675eb13914bba85c23498f906
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2024-06-24 09:14:48 -0600

    finally get a text editor working

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,20 +1,55 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import type { Editor as TinyMCEEditor } from 'tinymce';
 
 const TextEditor = () => {
-  const [input, setInput] = useState('');
-
-  const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInput(e.target.value);
+  const editorRef = useRef<TinyMCEEditor | null>(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
   };
 
   return (
-    <div>
-      <textarea rows={30} cols={70} value={input} onChange={handleInput} />
-      <div>
-        <button onClick={() => setInput('')}>Clear All</button>
-        <button>Save</button>
-      </div>
-    </div>
+    <>
+      <Editor
+        apiKey={import.meta.env.VITE_TINYMCE_KEY}
+        onInit={(_evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'preview',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'code',
+            'help',
+            'wordcount',
+          ],
+          toolbar:
+            'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
+      />
+      <button onClick={log}>Log editor content</button>
+    </>
   );
 };
 
