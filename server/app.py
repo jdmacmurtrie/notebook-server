aaf10cbf4c35d69b6bc2557808e04d271c3a49fd
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2024-06-26 14:29:56 -0600

    clean up server, experiment with sqlalchemy

--- server/app.py
+++ server/app.py
@@ -1,20 +1,67 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+""" read from a SQLite database and return data """
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os.path
 
-# instantiate the app
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy()
+# create the app
 app = Flask(__name__)
-app.config.from_object(__name__)
+# change string to the name of your database; add path if necessary
+db_name = 'sockmarket.db'
+# note - path is necessary for a SQLite db!!!
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, db_name)
 
-# enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# initialize the app with Flask-SQLAlchemy
+db.init_app(app)
+
+
+# each table in the database needs a class to be created for it
+# this class is named Sock because the database contains info about socks
+# and the table in the database is named: socks
+# db.Model is required - don't change it
+# identify all columns by name and their data type
+class Sock(db.Model):
+    __tablename__ = 'socks'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    style = db.Column(db.String)
+    color = db.Column(db.String)
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    updated = db.Column(db.String)
+
+# NOTHING BELOW THIS LINE NEEDS TO CHANGE
+# this route will test the database connection - and nothing more
+@app.route('/')
+def index():
+    try:
+        socks = db.session.execute(db.select(Sock)
+            .filter_by(style='knee-high')
+            .order_by(Sock.name)).scalars()
+
+        sock_text = '<ul>'
+        for sock in socks:
+            sock_text += '<li>' + sock.name + ', ' + sock.color + '</li>'
+        sock_text += '</ul>'
+        return sock_text
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
 
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
-
 
 if __name__ == '__main__':
     app.run()
