5c787899af4634d2a1edb456a2fdddb94204d49d
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2024-07-07 15:36:10 -0600

    add some styling

--- client/src/components/TextEditor.tsx
+++ client/src/components/TextEditor.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Editor } from '@tinymce/tinymce-react';
 import type { Editor as TinyMCEEditor } from 'tinymce';
+import './TextEditor.scss'
+
 import { AppDispatch, RootState } from '../store';
 import { addNewPage, updatePage } from '../pageSlice';
-import { useDispatch } from 'react-redux';
 
 const TextEditor = () => {
   const currentPage = useSelector((state: RootState) => state.page.currentPage)
   const dispatch = useDispatch<AppDispatch>()
 
-  const [title, setTitle] = useState('Title')
+  const [title, setTitle] = useState('')
   const editorRef = useRef<TinyMCEEditor | null>(null);
 
   useEffect(() => {
@@ -19,11 +20,11 @@ const TextEditor = () => {
     }
   })
 
-  const handleSubmit = () => {
+  const handleSave = () => {
     if (currentPage) {
       dispatch(updatePage({
         title,
-        // handleSubmit is only enabled if !!editorRef.current
+        // handleSave is only enabled if !!editorRef.current.getContent()
         // fallback is to satisfy TS
         body: editorRef.current?.getContent() || '',
         id: currentPage.id
@@ -38,45 +39,55 @@ const TextEditor = () => {
 
   return (
     <>
-      <input value={title} onChange={(e) => setTitle(e.target.value)} />
-      <div className="editor-wrapper">
-        <Editor
-          apiKey={import.meta.env.VITE_TINYMCE_KEY}
-          onInit={(_evt, editor) => (editorRef.current = editor)}
-          initialValue={currentPage?.body || ""}
-          init={{
-            height: 500,
-            menubar: false,
-            plugins: [
-              'advlist',
-              'autolink',
-              'lists',
-              'link',
-              'image',
-              'charmap',
-              'preview',
-              'anchor',
-              'searchreplace',
-              'visualblocks',
-              'code',
-              'fullscreen',
-              'insertdatetime',
-              'media',
-              'table',
-              'code',
-              'help',
-              'wordcount',
-            ],
-            toolbar:
-              'undo redo | blocks | ' +
-              'bold italic forecolor | alignleft aligncenter ' +
-              'alignright alignjustify | bullist numlist outdent indent | ' +
-              'removeformat | help',
-            content_style:
-              'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
-          }}
-        />
-        <button disabled={!editorRef.current} onClick={handleSubmit}>Submit</button>
+      <input
+        className='title'
+        value={title}
+        placeholder='Title'
+        aria-label='Title'
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <Editor
+        apiKey={import.meta.env.VITE_TINYMCE_KEY}
+        onInit={(_evt, editor) => (editorRef.current = editor)}
+        initialValue={currentPage?.body || ""}
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'preview',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'code',
+            'help',
+            'wordcount',
+          ],
+          toolbar:
+            'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
+      />
+      <div className='submit-button'>
+        <button
+          disabled={!title || !editorRef.current?.getContent()}
+          onClick={handleSave}>
+          Save
+        </button>
       </div>
     </>
   );
